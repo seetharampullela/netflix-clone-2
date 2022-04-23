4aa0ef50ce3944916eda358421ea45acd957153d
Sai Seetharama Pavan Kumar <98651745+seetharampullela@users.noreply.github.com>
2022-04-23 10:09:47 +0530

    fourth change to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
 import Header from '../Header'
 import Trending from '../Trending'
 
@@ -8,7 +9,6 @@ import Footer from '../Footer'
 import LoadingElement from '../LoaderElement'
 import Originals from '../Originals'
 
-
 const apiConstants = {
   initial: 'INITIAL',
   inProgress: 'INPROGRESS',
@@ -17,13 +17,58 @@ const apiConstants = {
 }
 
 class Home extends Component {
+  state = {
+    apiStatus: apiConstants.initial,
+    allTrendingVideos: [],
+  }
+
+  componentDidMount() {
+    this.getAllVideos()
+  }
+
+  getAllVideos = async () => {
+    this.setState({apiStatus: apiConstants.inProgress})
+
+    const url = 'https://apis.ccbp.in/movies-app/originals'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+
+    const response = await fetch(url, options)
+    if (response.ok) {
+      const data = await response.json()
+
+      const updatedVideosList = data.results.map(each => ({
+        id: each.id,
+        backdropPath: each.backdrop_path,
+        overview: each.overview,
+        posterPath: each.poster_path,
+        title: each.title,
+      }))
+
+      this.setState({
+        apiStatus: apiConstants.success,
+        allTrendingVideos: updatedVideosList,
+      })
+    } else {
+      this.setState({apiStatus: apiConstants.failure})
+    }
+  }
 
   render() {
     const renderSuccessView = () => {
       const {allTrendingVideos} = this.state
 
-      const backgroundImage = "https://res.cloudinary.com/dtjcxf7z5/image/upload/v1650251824/Mini%20Project%20Netflix%20Clone/Superman_gjemba.png"
-      
+      const homeHeaderItem =
+        allTrendingVideos[Math.floor(Math.random() * allTrendingVideos.length)]
+
+      const backgroundImage = homeHeaderItem.backdropPath
+      const titleOfHeader = homeHeaderItem.title
+      const overviewOfHeader = homeHeaderItem.overview
 
       return (
         <div
@@ -36,8 +81,8 @@ class Home extends Component {
         >
           <Header />
           <div className="home-header-content">
-            <h1 className="movie-details-name">Man of Steel</h1>
-            <p className="movie-details-description">Man of steel from superman franchise directed by Zack Snyder</p>
+            <h1 className="movie-details-name">{titleOfHeader}</h1>
+            <p className="movie-details-description">{overviewOfHeader}</p>
             <button type="button" className="movies-details-play-button">
               Play
             </button>
@@ -97,7 +142,6 @@ class Home extends Component {
                 {getResult()}
                 <h1 className="trending-heading">Trending Now</h1>
                 <Trending />
-                
                 <h1 className="trending-heading">Originals</h1>
                 <Originals />
               </div>
